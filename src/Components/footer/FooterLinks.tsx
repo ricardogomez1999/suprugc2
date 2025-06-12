a4decc75d55220cb5a4372baeda13440e456adf0
Ricardo Gomez <r.gomez1999@outlook.com>
2025-06-12 11:28:50 -0600

    fixing links

--- src/Components/footer/FooterLinks.tsx
+++ src/Components/footer/FooterLinks.tsx
@@ -74,7 +74,7 @@ export default function FooterLinks() {
             aria-label="Visit us on Facebook"
             className="text-gray-950 data-hover:text-gray-950/75"
             data-headlessui-state=""
-            href="https://facebook.com"
+            href="https://www.instagram.com/suprugc?igsh=YTJwZGJueWw1c3l2&utm_source=qr"
           >
             <svg viewBox="0 0 16 16" fill="currentColor" className="size-4">
               <path
@@ -84,7 +84,7 @@ export default function FooterLinks() {
               ></path>
             </svg>
           </Link>
-          <Link
+          {/* <Link
             target="_blank"
             aria-label="Visit us on X"
             className="text-gray-950 data-hover:text-gray-950/75"
@@ -94,13 +94,13 @@ export default function FooterLinks() {
             <svg viewBox="0 0 16 16" fill="currentColor" className="size-4">
               <path d="M12.6 0h2.454l-5.36 6.778L16 16h-4.937l-3.867-5.594L2.771 16H.316l5.733-7.25L0 0h5.063l3.495 5.114L12.6 0zm-.86 14.376h1.36L4.323 1.539H2.865l8.875 12.837z"></path>
             </svg>
-          </Link>
+          </Link> */}
           <Link
             target="_blank"
             aria-label="Visit us on LinkedIn"
             className="text-gray-950 data-hover:text-gray-950/75"
             data-headlessui-state=""
-            href="https://linkedin.com"
+            href="https://www.linkedin.com/company/suprugc/"
           >
             <svg viewBox="0 0 16 16" fill="currentColor" className="size-4">
               <path d="M14.82 0H1.18A1.169 1.169 0 000 1.154v13.694A1.168 1.168 0 001.18 16h13.64A1.17 1.17 0 0016 14.845V1.15A1.171 1.171 0 0014.82 0zM4.744 13.64H2.369V5.996h2.375v7.644zm-1.18-8.684a1.377 1.377 0 11.52-.106 1.377 1.377 0 01-.527.103l.007.003zm10.075 8.683h-2.375V9.921c0-.885-.015-2.025-1.234-2.025-1.218 0-1.425.966-1.425 1.968v3.775H6.233V5.997H8.51v1.05h.032c.317-.601 1.09-1.235 2.246-1.235 2.405-.005 2.851 1.578 2.851 3.63v4.197z"></path>
